c65e256d30662ccc6d8aa670fa19ef31271a00cc
maphe <sokaru91@gmail.com>
2019-09-09 00:03:37 -0700

    Add gitignore

--- e7-damage-calc/heroes.js
+++ e7-damage-calc/heroes.js
@@ -24,19 +24,19 @@ const heroes = {
       s1: {
         rate: 0.7,
         pow: 1,
-        flat: () => 0.085*elements.caster_max_hp.value(),
+        flat: () => 0.085*elements.caster_max_hp.value(), // 8.5% self max Health
         enhance: [0.05, 0, 0, 0.1, 0, 0.15]
       },
       s2: {
         rate: 0.8,
         pow: 1,
-        flat: () => 0.13*elements.caster_max_hp.value(),
+        flat: () => 0.13*elements.caster_max_hp.value(), // 13% self max Health
         enhance: [0.05, 0, 0.05, 0, 0, 0.1, 0.1]
       },
       s3: {
         rate: 0.3,
         pow: 1,
-        flat: () => 0.53571*(elements.caster_max_hp.value()-elements.caster_hp.value()),
+        flat: () => 0.53571*(elements.caster_max_hp.value()-elements.caster_hp.value()), // 0.53571 flat per 1 hp missing
         penetrate: () => 1.0,
       }
     }
