dfe8b77e26fa0a8fe918cf64a056b110dfea1646
maphe <sokaru91@gmail.com>
2020-04-09 17:08:42 -0700

    Info banner on Pavel's S3 pre-buff multi

--- e7-damage-calc/heroes.js
+++ e7-damage-calc/heroes.js
@@ -2628,6 +2628,7 @@ const heroes = {
     element: element.earth,
     classType: classType.ranger,
     form: [elements.caster_speed],
+    info: "Pavel's S3 is still calculated with pre-buff multipliers until the new multipliers (~25%) are datamined after 4/16 patch",
     skills: {
       s1: {
         rate: 0.9,
