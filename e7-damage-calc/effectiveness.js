d7e482992b37f00b98c6277e97618bfce9180e4d
maphe <sokaru91@gmail.com>
2020-11-08 12:49:22 -0800

    Add Hit Chance in Effectiveness Checker

--- e7-damage-calc/effectiveness.js
+++ e7-damage-calc/effectiveness.js
@@ -1,13 +1,15 @@
 
 
 const resolve = () => {
+  const hitChance = Number(document.getElementById('hit-chance').value)/100;
   const procChance = Number(document.getElementById('effect-chance').value)/100;
   const eff = Number(document.getElementById('effectiveness').value)/100;
   const effRes = Number(document.getElementById('effect-resistance').value)/100;
 
   const landChance = Math.max(Math.min(1 + eff - effRes, 0.85), 0);
   const resistChance = 1 - landChance;
-  const inflictChance = procChance * landChance;
+
+  const inflictChance = hitChance * procChance * landChance;
 
   document.getElementById('passing-res').innerText = Math.round(landChance*100).toString();
   document.getElementById('resist').innerText = Math.round(resistChance*100).toString();
