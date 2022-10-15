bbda61b5d862c87a99166015f2f2e4ed3ed85e22
tyopoyt <dcrouseIII@gmail.com>
2022-10-15 09:56:59 -0400

    Style output colors, Improve edge case handling

--- docs/speed-tuner/speed-tuner.js
+++ docs/speed-tuner/speed-tuner.js
@@ -1,9 +1,42 @@
+// get elements from page
+//should be fine here since no elements are ever added to or removed from the DOM after inital load
+const slowMaxSpeedBox = document.getElementById('slow-max-speed-box');
+const fastMinSpeedBox = document.getElementById('fast-min-speed-box');
+const slowMaxSpeedOutput = document.getElementById('slow-max-speed');
+const fastMinSpeedOutput = document.getElementById('fast-min-speed');
+const recommendationOutput =  document.getElementById('recommendation');
+
+const slowerUnitSpeedInput = document.getElementById('slow-unit-speed');
+const fasterUnitSpeedInput = document.getElementById('fast-unit-speed');
+const slowerUnitPushInput = document.getElementById('slow-unit-push');
+const fasterUnitPushInput = document.getElementById('fast-unit-push');
+const fasterUnitTurnsInput = document.getElementById('fast-unit-turns');
+
+// style output text and outlines
+updateClasses = (correctTune = false) => {
+    if (correctTune) {
+        slowMaxSpeedBox.classList.replace('border-danger', 'border-success');
+        slowMaxSpeedOutput.classList.replace('text-danger', 'text-success');
+
+        fastMinSpeedBox.classList.replace('border-danger', 'border-success');
+        fastMinSpeedOutput.classList.replace('text-danger', 'text-success');
+
+    } else {
+        slowMaxSpeedBox.classList.replace('border-success', 'border-danger');
+        slowMaxSpeedOutput.classList.replace('text-success', 'text-danger');
+
+        fastMinSpeedBox.classList.replace('border-success', 'border-danger');
+        fastMinSpeedOutput.classList.replace('text-success', 'text-danger');
+    }
+}
+
 const resolve = () => {
-    const slowerUnitSpeed = Number(document.getElementById('slow-unit-speed').value);
-    const fasterUnitSpeed = Number(document.getElementById('fast-unit-speed').value);
-    const slowerUnitPush = Number(document.getElementById('slow-unit-push').value);
-    const fasterUnitPush = Number(document.getElementById('fast-unit-push').value);
-    const fasterUnitTurns = Number(document.getElementById('fast-unit-turns').value);
+    const slowerUnitSpeed = Number(slowerUnitSpeedInput.value);
+    const slowerUnitPush = Number(slowerUnitPushInput.value);
+    
+    const fasterUnitSpeed = Number(fasterUnitSpeedInput.value);
+    const fasterUnitPush = Number(fasterUnitPushInput.value);
+    const fasterUnitTurns = Number(fasterUnitTurnsInput.value);
     
     /* 
      * FORMULA: fast Speed = slow Speed * fast CR / slow CR
@@ -16,23 +49,30 @@ const resolve = () => {
     const CRRatio = fasterUnitCR / slowerUnitCR;
 
     // take the floor of max speed and ceil of min speed for safety since we can't have fractional speed
-    const slowUnitMaxSpeed = Math.max(Math.floor(fasterUnitSpeed / CRRatio), 0);
-    const fastUnitMinSpeed =  Math.max(Math.ceil(slowerUnitSpeed * CRRatio), 0);
+    const slowUnitMaxSpeed = Math.floor(fasterUnitSpeed / CRRatio);
+    const fastUnitMinSpeed =  Math.ceil(slowerUnitSpeed * CRRatio);
 
+    const formattedMaxSpeed = slowUnitMaxSpeed <= 0 || slowUnitMaxSpeed == 'Infinity' ? 'Impossible' : slowUnitMaxSpeed.toString();
+    const formattedMinSpeed = fastUnitMinSpeed <= 0 || fastUnitMinSpeed == 'Infinity' ? 'Impossible' : fastUnitMinSpeed.toString();
 
     let recommendation = '';
 
     if (slowerUnitSpeed > slowUnitMaxSpeed || fasterUnitSpeed < fastUnitMinSpeed) {
-        recommendation = 'Units are improperly tuned, and the desired turn order isn\'t guaranteed.' +
-                         `\nSlower unit needs at least ${slowerUnitSpeed - slowUnitMaxSpeed} less speed or Faster Unit needs at least ${fastUnitMinSpeed - fasterUnitSpeed} more speed.`
+        updateClasses(false);
+        recommendation = 'Units are improperly tuned, and the desired turn order isn\'t guaranteed.\n' +
+                         (formattedMaxSpeed !== 'Impossible' ? `Slower unit needs at least ${slowerUnitSpeed - slowUnitMaxSpeed} less speed` : '') +
+                         (!(formattedMaxSpeed === 'Impossible' || formattedMinSpeed === 'Impossible') ? ' or ' : '') +
+                         (formattedMinSpeed !== 'Impossible' ? `Faster Unit needs at least ${fastUnitMinSpeed - fasterUnitSpeed} more speed.` : '');
     } else {
+        updateClasses(true);
         recommendation = 'Units are properly tuned, and the faster unit will always move first.'
         if (slowerUnitSpeed < slowUnitMaxSpeed || fasterUnitSpeed > fastUnitMinSpeed) {
-            recommendation += `\nSlower Unit can have up to ${slowUnitMaxSpeed - slowerUnitSpeed} more speed or Faster Unit can have up to ${fasterUnitSpeed - fastUnitMinSpeed} less speed.`;
+            recommendation += `\nSlower Unit can have up to ${slowUnitMaxSpeed - slowerUnitSpeed} more speed` +
+                              ` or Faster Unit can have up to ${fasterUnitSpeed - fastUnitMinSpeed} less speed.`;
         }
     }
   
-    document.getElementById('slow-max-speed').innerText = slowUnitMaxSpeed.toString();
-    document.getElementById('fast-min-speed').innerText = fastUnitMinSpeed.toString();
-    document.getElementById('recommendation').innerText = recommendation;
+    slowMaxSpeedOutput.innerText = formattedMaxSpeed;
+    fastMinSpeedOutput.innerText = formattedMinSpeed;
+    recommendationOutput.innerText = recommendation;
   }
